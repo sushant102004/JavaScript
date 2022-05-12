80234efedebc3135152e759816430c595295a907
sushant102004 <sushant.dhiman9812@gmail.com>
2022-05-12 17:56:58 +0530

    12-05-2022

--- 008-CallBindApply/app.js
+++ 008-CallBindApply/app.js
@@ -1,13 +1,40 @@
-var john = {
-    name: 'John',
-    age: 22,
-    presentation: function (style, time) {
+var sushant = {
+    name: 'Sushant',
+    age: 18,
+    intro: function (style, time) {
         if (style == 'formal') {
-            console.log('Good ' + time + ' morning ladies and gentleman. I\'m ' + this.name + ' and I\'m ' + this.age + ' years old.')
-        } else if(style == 'friendly'){
-            console.log('Hello guys, I\'m ' + this.name + ' and I\'m ' + this.age + ' years old')
+            return console.log(time + ' Ladies and Gentlemen, It\'s ' + this.name + ' here.')
+        } else if (style == 'friendly') {
+            return console.log(time + ' guys, It\'s ' + this.name + ' here.')
         }
     }
 }
 
-john.presentation('formal', 'evening')
+var emily = {
+    name: 'Emily',
+    age: 17
+}
+
+// sushant.intro('friendly', 'Good Morning')
+// This is called method borowing.
+// sushant.intro.call(emily, 'formal', 'Good Afternoon')
+// sushant.intro.apply(emily, ['formal', 'Good Evening'])
+
+// Bind Method will return a function
+var sushantFormal = sushant.intro.bind(sushant, 'formal')
+// sushantFormal('Good Evening')
+
+// Real World Example
+var birthYears = [1989, 2004, 2003, 1980]
+
+function calculateArray(arr, fn){
+    var newArr = []
+    for(var i = 0; i < arr.length; i++){
+        newArr.push(fn(arr[i]))
+    }
+    return newArr
+}
+
+function calculateAge(element){
+    return 2022 - element
+}
